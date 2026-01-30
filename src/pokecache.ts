1dc5a67d277a3d39538bbf971d6f5bb065d3a499
5PenguinsInATrenchCoat <draven2121@gmail.com>
2026-01-30 09:55:25 -0800

    cache work

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -6,8 +6,23 @@ export type CacheEntry<T> = {
 export class Cache {
     #cache = new Map<string, CacheEntry<any>>();
 
-    add<T>() {
-        
+    add<T>(key: string, val: T) {
+        this.#cache.set(key, {
+            createdAt: Date.now(),
+            val: val
+        });
+    }
+
+    get<T>(key: string): T | undefined {
+        return this.#cache.get(key)?.val;
+    }
+
+    #reap() {
+        for (const [key, entry] of this.#cache.entries()) {
+            if (entry.createdAt < Date.now() - this.#interval) {
+                this.#cache.delete(entry.key);
+            }
+        }
     }
 }
 
