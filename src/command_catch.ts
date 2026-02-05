8b417982b065d35775fe125670d70c8b33890e0e
5PenguinsInATrenchCoat <draven2121@gmail.com>
2026-02-05 15:46:14 -0800

    Implement commandCatch function in command_catch.ts
    Add commandCatch function to handle Pokémon commands.

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -1,1 +1,10 @@
 import { State } from "./state.js";
+
+export async function commandCatch(state: State, ...args: string[]) {
+  const pokeName = args[0].toLowerCase()
+  const pokemon = //continue here
+    
+  console.log("Throwing a Pokeball at ${pokeName}...");
+
+  
+}
