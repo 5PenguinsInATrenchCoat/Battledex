725d4a21e444d9c3cbaebfb32041eed2ac7362a9
5PenguinsInATrenchCoat <draven2121@gmail.com>
2026-02-10 09:55:40 -0800

    first

---
+++ src/command_buildteam.ts
@@ -0,0 +1,6 @@
+import { State } from "./state.js";
+
+export async function commandBuildTeam(state: State, ...args: string[]) {
+    console.log("Enter the name of the Pokemon you wish to add:");
+    
+    )
