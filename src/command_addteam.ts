cfb7de257ed5abc92b889403fb720779cf6d7a79
5PenguinsInATrenchCoat <draven2121@gmail.com>
2026-02-10 11:01:41 -0800

    addteam command

---
+++ src/command_addteam.ts
@@ -0,0 +1,29 @@
+import { State } from "./state.js";
+import type { Pokemon } from "./pokeapi.js";
+
+export async function commandBuildTeam(state: State, ...args: string[]) {
+    if (state.poketeam.length >= 6) { // Checked before anything is processed
+        console.log("Your party is full, please remove a member before adding a new one");
+        return // Exits early if party is already full
+    }
+    const pokename = args[0]
+    let newMember: Pokemon | null = null; //Newmember can be a Pokemon object or null, initially sets it to null
+
+    if (!pokename) { // Only runs when addteam is called with no other input
+        console.log("Usage: addteam <pokemon name>")
+        return
+    }
+    
+    try {
+        newMember = await state.pokeAPI.fetchPokemon(pokename);
+    } catch { // If the name isn't found through pokeAPI
+        console.log("Please enter a valid pokemon name");
+        return
+    }
+
+    if (newMember in state.poketeam) { // No commonly used format allows for multiple of the same pokemon, so multiples will never be allowed
+        console.log(`${newMember.name} is already in your team!`)
+    } else {
+        state.poketeam.push(newMember);
+    }
+}
--- src/command_buildteam.ts
+++
@@ -1,6 +0,0 @@
-import { State } from "./state.js";
-
-export async function commandBuildTeam(state: State, ...args: string[]) {
-    console.log("Enter the name of the Pokemon you wish to add:");
-    
-    )
